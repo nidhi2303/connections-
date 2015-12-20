9a3e4cdd7088b07aa6363bbb12b0f16534e66972
nidhi2303 <nidhi2303vora@gmail.com>
2015-12-20 21:28:18 +0530

    hello

--- connection.js
+++ connection.js
@@ -1,17 +1,22 @@
 var mysql = require('mysql');
  
-var connection = mysql.createConnection({
- host: 'localhost',
-user: 'root',
-password :'nidhi123',
-database : 'db',
-})
-var log = {
-username : "charmy",
-pass : "hello"
+var connection = mysql.createConnection
+(
+	{
+ 		host: 'localhost',
+		user: 'root',
+		password :'nidhi123',
+		database : 'db',
+	}
+)
+var log = 
+{
+	username : "charmy123",
+	pass : "hello"
 }
-connection.query('INSERT INTO login SET ?', log, function(err,res){
-  if(err) throw err;
-
-  console.log('Last insert ID:', res.insertId);
-});
+connection.query('INSERT INTO login SET ?', log, function(err,res)
+{
+  	if(err) throw err;
+ 	console.log('Last insert ID:', res.insertId);
+}
+);
